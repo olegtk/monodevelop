3976ee5af8b30eebce911658f8a769be1adba644
Oleg Tkachenko <olegtk@microsoft.com>
2018-01-16 23:24:42 -0800

    Migrate to modern editor commanding

--- main/src/addins/CSharpBinding/Razor/RoslynCommandTarget.cs
+++ main/src/addins/CSharpBinding/Razor/RoslynCommandTarget.cs
@@ -4,19 +4,19 @@
 //
 
 using System;
-using Microsoft.CodeAnalysis.Editor;
-using Microsoft.CodeAnalysis.Editor.Commands;
 using Microsoft.CodeAnalysis.Editor.Shared.Utilities;
 using Microsoft.CodeAnalysis.Utilities;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.Commanding;
+using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
 using MonoDevelop.Ide.Composition;
 
 namespace Microsoft.VisualStudio.Platform
 {
     public class RoslynCommandTarget
     {
-        internal ICommandHandlerService CurrentHandlers { get; set; }
+        internal IEditorCommandHandlerService CurrentHandlers { get; set; }
 
         internal ITextBuffer _languageBuffer;
         internal ITextView _textView;
@@ -30,13 +30,11 @@ namespace Microsoft.VisualStudio.Platform
 
         private RoslynCommandTarget(ITextView textView, ITextBuffer languageBuffer)
         {
-            ICommandHandlerServiceFactory commandHandlerServiceFactory = CompositionManager.GetExportedValue<ICommandHandlerServiceFactory>();
+            var commandHandlerServiceFactory = CompositionManager.GetExportedValue<IEditorCommandHandlerServiceFactory>();
 
             if (commandHandlerServiceFactory != null)
             {
-                commandHandlerServiceFactory.Initialize (languageBuffer.ContentType.TypeName);
-
-                CurrentHandlers = commandHandlerServiceFactory.GetService (languageBuffer);
+                CurrentHandlers = commandHandlerServiceFactory.GetService(textView, languageBuffer);
             }
 
             _languageBuffer = languageBuffer;
@@ -50,72 +48,62 @@ namespace Microsoft.VisualStudio.Platform
 
         public void ExecuteTypeCharacter(char typedChar, Action lastHandler)
         {
-            CurrentHandlers?.Execute (_languageBuffer.ContentType,
-                args: new TypeCharCommandArgs (_textView, _languageBuffer, typedChar),
-                lastHandler: lastHandler);
+            CurrentHandlers?.Execute ((view, buffer) => new TypeCharCommandArgs (_textView, _languageBuffer, typedChar),
+				nextCommandHandler: lastHandler);
         }
 
         public void ExecuteTab (Action executeNextCommandTarget)
         {
-            CurrentHandlers.Execute (_languageBuffer.ContentType,
-                args: new TabKeyCommandArgs (_textView, _languageBuffer),
-                lastHandler: executeNextCommandTarget);
+            CurrentHandlers.Execute ((view, buffer) => new TabKeyCommandArgs (_textView, _languageBuffer),
+                nextCommandHandler: executeNextCommandTarget);
         }
 
         public void ExecuteBackspace(Action executeNextCommandTarget)
         {
-            CurrentHandlers.Execute (_languageBuffer.ContentType,
-                args: new BackspaceKeyCommandArgs (_textView, _languageBuffer),
-                lastHandler: executeNextCommandTarget);
+            CurrentHandlers.Execute ((view, buffer) => new BackspaceKeyCommandArgs (_textView, _languageBuffer),
+                nextCommandHandler: executeNextCommandTarget);
         }
 
         public void ExecuteDelete(Action executeNextCommandTarget)
         {
-            CurrentHandlers.Execute (_languageBuffer.ContentType,
-                args: new DeleteKeyCommandArgs (_textView, _languageBuffer),
-                lastHandler: executeNextCommandTarget);
+            CurrentHandlers.Execute ((view, buffer) => new DeleteKeyCommandArgs (_textView, _languageBuffer),
+                nextCommandHandler: executeNextCommandTarget);
         }
 
         public void ExecuteReturn(Action executeNextCommandTarget)
         {
-            CurrentHandlers.Execute (_languageBuffer.ContentType,
-                args: new ReturnKeyCommandArgs (_textView, _languageBuffer),
-                lastHandler: executeNextCommandTarget);
+            CurrentHandlers.Execute ((view, buffer) => new ReturnKeyCommandArgs (_textView, _languageBuffer),
+                nextCommandHandler: executeNextCommandTarget);
         }
 
         public void ExecuteUp (Action executeNextCommandTarget)
         {
-            CurrentHandlers.Execute (_languageBuffer.ContentType,
-                args: new UpKeyCommandArgs (_textView, _languageBuffer),
-                lastHandler: executeNextCommandTarget);
+            CurrentHandlers.Execute ((view, buffer) => new UpKeyCommandArgs (_textView, _languageBuffer),
+                nextCommandHandler: executeNextCommandTarget);
         }
 
         public void ExecuteDown (Action executeNextCommandTarget)
         {
-            CurrentHandlers.Execute (_languageBuffer.ContentType,
-                args: new DownKeyCommandArgs (_textView, _languageBuffer),
-                lastHandler: executeNextCommandTarget);
+            CurrentHandlers.Execute ((view, buffer) => new DownKeyCommandArgs (_textView, _languageBuffer),
+                nextCommandHandler: executeNextCommandTarget);
         }
 
         public void ExecuteUncommentBlock(Action executeNextCommandTarget)
         {
-            CurrentHandlers.Execute (_languageBuffer.ContentType,
-                args: new UncommentSelectionCommandArgs (_textView, _languageBuffer),
-                lastHandler: executeNextCommandTarget);
+            CurrentHandlers.Execute ((view, buffer) => new UncommentSelectionCommandArgs (_textView, _languageBuffer),
+                nextCommandHandler: executeNextCommandTarget);
         }
 
         public void ExecuteCommentBlock(Action executeNextCommandTarget)
         {
-            CurrentHandlers.Execute (_languageBuffer.ContentType,
-                args: new CommentSelectionCommandArgs (_textView, _languageBuffer),
-                lastHandler: executeNextCommandTarget);
+            CurrentHandlers.Execute ((view, buffer) => new CommentSelectionCommandArgs (_textView, _languageBuffer),
+                nextCommandHandler: executeNextCommandTarget);
         }
 
         public void ExecuteInvokeCompletionList (Action executeNextCommandTarget)
         {
-            CurrentHandlers.Execute (_languageBuffer.ContentType,
-                args: new InvokeCompletionListCommandArgs (_textView, _languageBuffer),
-                lastHandler: executeNextCommandTarget);
+            CurrentHandlers.Execute ((view, buffer) => new InvokeCompletionListCommandArgs (_textView, _languageBuffer),
+                nextCommandHandler: executeNextCommandTarget);
         }
     }
 }
